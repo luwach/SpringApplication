a29def5b84f6e3a2686e695197bf640ebbf6a7a9
Łukasz Wachowski <lwach87@gmail.com>
2018-03-01 17:20:08 +0100

    Example using spring data models

--- src/com/spring/web/controllers/OffersController.java
+++ src/com/spring/web/controllers/OffersController.java
@@ -1,13 +1,17 @@
 package com.spring.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class OffersController {
 	
 	@RequestMapping("/")
-	public String showHome() {
+	public String showHome(Model model) {
+		
+		model.addAttribute("name", "Tiffany");
+		
 		return "home";
 	}
 
