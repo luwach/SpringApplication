fabe99b2603d996fa9901d7d9b0fb34286935642
Łukasz Wachowski <lwach87@gmail.com>
2018-03-01 17:06:40 +0100

    Controller and jsp view resolver added

---
+++ src/com/spring/web/controllers/OffersController.java
@@ -0,0 +1,14 @@
+package com.spring.web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OffersController {
+	
+	@RequestMapping("/")
+	public String showHome() {
+		return "home";
+	}
+
+}
