e7d3fb1e9378a8ebb48ab8f1a9674a7163a786eb
Łukasz Wachowski <lwach87@gmail.com>
2018-03-02 13:31:37 +0100

    Form validation added

--- src/com/spring/web/controllers/OffersController.java
+++ src/com/spring/web/controllers/OffersController.java
@@ -2,9 +2,13 @@ package com.spring.web.controllers;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -33,9 +37,20 @@ public class OffersController {
 	}
 
 	@RequestMapping(value = "/docreate", method = RequestMethod.POST)
-	public String doCreate(Model model, Offer offer) {
-		
-		System.out.println(offer);
+	public String doCreate(Model model, @Valid Offer offer, BindingResult result) {
+
+		if (result.hasErrors()) {
+			System.out.println("Form does not validate.");
+
+			List<ObjectError> errors = result.getAllErrors();
+
+			for (ObjectError error : errors) {
+				System.out.println(error.getDefaultMessage());
+			}
+
+		} else {
+			System.out.println("Form validated.");
+		}
 
 		return "offercreated";
 	}
--- src/com/spring/web/dao/Offer.java
+++ src/com/spring/web/dao/Offer.java
@@ -1,10 +1,21 @@
 package com.spring.web.dao;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class Offer {
 
 	private int id;
+	
+	@Size(min=5, max=100, message="Name must be between 5 and 100 characters.")
 	private String name;
+	
+	@NotNull
+	@Pattern(regexp=".*\\@.*\\..*", message="This does not appear to be a valid email address")
 	private String email;
+	
+	@Size(min=5, max=100, message="Text must be between 20 and 255 characters.")
 	private String text;
 
 	public Offer() {
